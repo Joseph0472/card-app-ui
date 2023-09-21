e4f327f868752ac818c5e308bfe268228d05fb6b
Ajon <jiangpeng0472@gmail.com>
2023-09-21 15:13:31 +1200

    Adjust

--- src/views/HomeScreen.tsx
+++ src/views/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from '../components/Header';
 import { Content } from '../components/Content';
+import { Form } from '../components/Form';
 
 function HomeScreen() {
     return (
@@ -11,6 +12,7 @@ function HomeScreen() {
                     FirstName: "Joseph",
                     LastName: "Jiang"
                 }}/>
+                <Form/>
             </div>
         </div>
     );
